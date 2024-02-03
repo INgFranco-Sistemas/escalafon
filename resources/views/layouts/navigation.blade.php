47d47e541075609b09951e095b95337926d3e383
Franco <andre.garcia.huanuco@gmail.com>
2024-02-02 23:05:01 -0500

    v8

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+{{-- <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <div class=" max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
@@ -88,4 +88,4 @@
             </div>
         </div>
     </div>
-</nav>
+</nav> --}}
